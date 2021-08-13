0188eba42f1079c36179b35f3298d26b31d29862
muskan agarwal <muskanagarwal1906@gmail.com>
2021-08-13 06:14:06 +0530

    [feat] stubs text and icons add

--- src/Pages/First.tsx
+++ src/Pages/First.tsx
@@ -7,15 +7,22 @@ import img2 from "../Assets/img/landingImage_2.svg";
 // import tick from "../Assets/img/tick.png";
 import gif from "../Assets/img/landing-gif.gif";
 import Header from "../Components/Header";
+import { useHistory } from "react-router-dom";
 import databaseStorage from "../Assets/img/dbimg/database-storage.png";
 import sql from "../Assets/img/dbimg/sql.png";
 import mysql from "../Assets/img/dbimg/mysql.png";
 import connection from "../Assets/img/dbimg/connection.png";
 import Grid from "@material-ui/core/Grid";
 export default function First() {
+  let history = useHistory();
   return (
     <div>
-      <Grid container justifyContent="center"   alignItems="center">
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ paddingBottom: "3rem" }}
+      >
         <Grid item md={4}>
           <Header />
           <p className="landing-text-heading">
@@ -27,47 +34,64 @@ export default function First() {
             your daily use and hasslefree
           </p>
           {/* <img src={tick} alt="tick"/> */}
-          <p className="landing-features">Progarmming environment not required</p>
+          <p className="landing-features">
+            Progarmming environment not required
+          </p>
           <p className="landing-features">Easy to use & good user experience</p>
-          <p className="landing-features" >Multi language support</p>
+          <p className="landing-features">Multi language support</p>
           <Grid container justifyContent="space-around">
-            <Grid item md={3}>
+            <Grid item md={3} style={{ marginTop: "2rem" }}>
               <CustomButton
                 border={true}
                 text="Run Online Editor"
-                onClick={() => {}}
+                onClick={() => {
+                  history.push("/editor");
+                }}
               />
             </Grid>
-            <Grid item md={3}>
-              <CustomButton
-                border={false}
-                text="Run Online Editor"
-                onClick={() => {}}
-              />
+            <Grid item md={3} style={{ marginTop: "2rem" }}>
+              <CustomButton border={false} text="Login" onClick={() => {}} />
             </Grid>
           </Grid>
         </Grid>
         <Grid item md={6} >
-          <img src={gif} alt="editor-gif"  width="500" height="400" className="landing-gif"/>
+          <img
+            src={gif}
+            alt="editor-gif"
+            width="500"
+            height="400"
+            className="landing-gif"
+            style={{ marginLeft: "12rem" }}
+          />
         </Grid>
 
-        <Grid item md={12}  style={{marginLeft:"3rem", marginTop:"5rem"}}>
+        <Grid item md={12} style={{ marginLeft: "3rem", marginTop: "5rem" }}>
           <Roww
             img={img}
             direction="row"
-            title={"Hello World"}
+            title={"Feature rich and customizable editor!"}
             description={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus dut lobortis infelis et ex mattis pellentesque. Nullam in volutpat neque. Nulla in consectetur purus. Maecenas mattis ante sed neque vestibulum fermentum. Proin quam justo, auctor ac consequat id, dapibus eu libero. Vestibulum ut elit erat. Sed accumsan egestas rhoncus. Pellentesque porttitor cursus metus sed ornare.\n"
+              "A SQL editor with syntax highlighting capabilities, highly customizable with support for over 30 themes and growing. " +
+              "Customize as per your taste and enjoy the experience."
             }
           />
         </Grid>
-        <Grid item md={12} style={{marginLeft:"8rem", marginTop:"5rem"}}>
+        <Grid
+          item
+          md={12}
+          style={{
+            marginLeft: "8rem",
+            marginTop: "5rem",
+            marginBottom: "5rem",
+          }}
+        >
           <Roww
             img={img2}
             direction="row-reverse"
-            title={"Hello World"}
+            title={"Test your SQL code with ease"}
             description={
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus dut lobortis infelis et ex mattis pellentesque. Nullam in volutpat neque. Nulla in consectetur purus. Maecenas mattis ante sed neque vestibulum fermentum. Proin quam justo, auctor ac consequat id, dapibus eu libero. Vestibulum ut elit erat. Sed accumsan egestas rhoncus. Pellentesque porttitor cursus metus sed ornare.\n"
+              "The portal allows you to quickly test out your SQL Queries, without any burdens of managing any SQL servers." +
+              " Run anytime from anywhere! "
             }
           />
         </Grid>
@@ -79,6 +103,7 @@ export default function First() {
             height="100"
           />
         </Grid>
+
         <Grid item md={3}>
           <img src={sql} alt="SQL" width="100" height="100" />
         </Grid>
--- src/Pages/Second.tsx
+++ src/Pages/Second.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Header from "../Components/Header";
 import AceEditor from "react-ace";
-import CustomButton from "../Components/Button/button";
+import IconButton from "@material-ui/core/IconButton";
 import "ace-builds/src-noconflict/mode-mysql";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/theme-ambiance";
@@ -44,74 +44,100 @@ import "ace-builds/src-noconflict/theme-vibrant_ink";
 import "ace-builds/src-noconflict/theme-xcode";
 import Grid from "@material-ui/core/Grid";
 import TitleInput from "../Components/TitleInput/titleInput";
+import run from "../Assets/img/run.png";
+import save from "../Assets/img/save.png";
 import "./Second.css";
+import axios from "axios";
 
 export default function Second() {
-  const [themeName, setThemeName] = useState("monokai");
-  return (
-    <div>
-      <Header />
-      <Grid container alignItems={"center"}>
-        <Grid item md={3}>
-          <TitleInput placeholder={"Enter Title"} />
-        </Grid>
+  const [themeName, setThemeName] = useState("monokai"); //updating the theme of the editor when option is clicked
 
-        <Grid item md={3}>
-          <select
-            id="ide-pattern"
-            onChange={(e) => {
-              let theme = e.target.value;
-              setThemeName(theme);
-            }}
-          >
-            <option value="monokai">monokai</option>
-            <option value="github">github</option>
-            <option value="ambiance">ambiance</option>
-            <option value="chaos">chaos</option>
-            <option value="chrome">chrome</option>
-            <option value="clouds">clouds</option>
-            <option value="clouds_midnight">clouds_midnight</option>
-            <option value="cobalt">cobalt</option>
-            <option value="crimson_editor">crimson_editor</option>
-            <option value="dawn">dawn</option>
-            <option value="dracula">dracula</option>
-            <option value="dreamweaver">dreamweaver</option>
-            <option value="eclipse">eclipse</option>
-            <option value="github">github</option>
-            <option value="gob">gob</option>
-            <option value="gruvbox">gruvbox</option>
-            <option value="idle_fingers">idle_fingers</option>
-            <option value="iplastic">iplastic</option>
-            <option value="katzenmilch">katzenmilch</option>
-            <option value="kr_theme">kr_theme</option>
-            <option value="kuroir">kuroir</option>
-            <option value="merbivore">merbivore</option>
-            <option value="merbivore_soft">merbivore_soft</option>
-            <option value="mono_industrial">mono_industrial</option>
-            <option value="nord_dark">nord_dark</option>
-            <option value="pastel_on_dark">pastel_on_dark</option>
-            <option value="solarized_dark">solarized_dark</option>
-            <option value="solarized_light">solarized_light</option>
-            <option value="sqlserver">sqlserver</option>
-            <option value="terminal">terminal</option>
-            <option value="textmate">textmate</option>
-            <option value="tomorrow">tomorrow</option>
-            <option value="tomorrow_night">tomorrow_night</option>
-            <option value="tomorrow_night_blue">tomorrow_night_blue</option>
-            <option value="tomorrow_night_bright">tomorrow_night_bright</option>
-            <option value="tomorrow_night_eighties">
-              tomorrow_night_eighties
-            </option>
-            <option value="twilight">twilight</option>
-            <option value="vibrant_ink">vibrant_ink</option>
-            <option value="xcode">xcode</option>
-          </select>
+  const QueryCall = async () => {
+    try {
+      let response = await axios.post(
+        "https://c293c534-d126-4141-a887-f76861baa1df.mock.pstmn.io/sql-result" //create a stub for customer data in csv
+      );
+      console.log(response);
+    } catch (err) {
+      alert("Error");
+    }
+  };
+  return (
+    <div
+    // style={{
+    //   backgroundImage: `url(${editorbackground})`,
+    //   backgroundSize: "cover",
+    // }}
+    >
+      <Grid container alignItems={"center"} style={{ marginTop: "3rem" }}>
+        <Grid item md={4}>
+          <Grid container>
+            <Grid item md={4}>
+              <Header />
+            </Grid>
+            <Grid item md={8}>
+              <select
+                id="ide-pattern"
+                onChange={(e) => {
+                  let theme = e.target.value;
+                  setThemeName(theme);
+                }}
+              >
+                <option value="monokai">monokai</option>
+                <option value="github">github</option>
+                <option value="ambiance">ambiance</option>
+                <option value="chaos">chaos</option>
+                <option value="chrome">chrome</option>
+                <option value="clouds">clouds</option>
+                <option value="clouds_midnight">clouds_midnight</option>
+                <option value="cobalt">cobalt</option>
+                <option value="crimson_editor">crimson_editor</option>
+                <option value="dawn">dawn</option>
+                <option value="dracula">dracula</option>
+                <option value="dreamweaver">dreamweaver</option>
+                <option value="eclipse">eclipse</option>
+                <option value="github">github</option>
+                <option value="gob">gob</option>
+                <option value="gruvbox">gruvbox</option>
+                <option value="idle_fingers">idle_fingers</option>
+                <option value="iplastic">iplastic</option>
+                <option value="katzenmilch">katzenmilch</option>
+                <option value="kr_theme">kr_theme</option>
+                <option value="kuroir">kuroir</option>
+                <option value="merbivore">merbivore</option>
+                <option value="merbivore_soft">merbivore_soft</option>
+                <option value="mono_industrial">mono_industrial</option>
+                <option value="nord_dark">nord_dark</option>
+                <option value="pastel_on_dark">pastel_on_dark</option>
+                <option value="solarized_dark">solarized_dark</option>
+                <option value="solarized_light">solarized_light</option>
+                <option value="sqlserver">sqlserver</option>
+                <option value="terminal">terminal</option>
+                <option value="textmate">textmate</option>
+                <option value="tomorrow">tomorrow</option>
+                <option value="tomorrow_night">tomorrow_night</option>
+                <option value="tomorrow_night_blue">tomorrow_night_blue</option>
+                <option value="tomorrow_night_bright">
+                  tomorrow_night_bright
+                </option>
+                <option value="tomorrow_night_eighties">
+                  tomorrow_night_eighties
+                </option>
+                <option value="twilight">twilight</option>
+                <option value="vibrant_ink">vibrant_ink</option>
+                <option value="xcode">xcode</option>
+              </select>
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item md={2} style={{ marginLeft: "auto" }}>
-          <CustomButton border={true} text="Save" onClick={() => {}} />
-        </Grid>
-        <Grid item md={2} style={{ marginLeft: "auto" }}>
-          <CustomButton border={false} text="Run" onClick={() => {}} />
+        <Grid item md={3} style={{ marginLeft: "auto" }}>
+          <IconButton>
+            <img src={run} alt="run-command" height="70" width="70" />
+          </IconButton>
+
+          <IconButton>
+            <img src={save} alt="save-command" height="50" width="50" />
+          </IconButton>
         </Grid>
       </Grid>
       <Grid container>
@@ -121,7 +147,7 @@ export default function Second() {
               height: "70vh",
               width: "90%",
               marginLeft: "3rem",
-              marginTop: "2rem",
+              marginTop: "1rem",
               borderRadius: "16px",
             }}
             placeholder="Write Your SQL Queries"
@@ -143,7 +169,7 @@ export default function Second() {
         </Grid>
         <Grid item md={6}>
           <TitleInput
-            style={{ width: "100%", height: "90%", marginTop: "2rem" }}
+            style={{ width: "90%", height: "86%", marginTop: "2rem" }}
           />
         </Grid>
       </Grid>
